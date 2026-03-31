9c18aaa3367efde772a2e01f5e0ad0c670bdafc2
Arbaj Ansari <arbaj897ansari@gmail.com>
2026-03-31 13:41:52 +0530

    feat(chore): setup i18n ally

--- app/layoutWrapper.tsx
+++ app/layoutWrapper.tsx
@@ -1,9 +1,21 @@
 "use client"
 import { App, Navbar } from "konsta/react"
+import { NextIntlClientProvider } from "next-intl";
+import { AbstractIntlMessages } from "next-intl";
 
-export const LayoutWrapper = ({ children }: { children: React.ReactNode }) => {
-    return <App>
-        <Navbar title="My App" />
-        {children}
-    </App>
+export const LayoutWrapper = ({ 
+  children, 
+  locale, 
+  messages 
+}: { 
+  children: React.ReactNode;
+  locale: string;
+  messages: AbstractIntlMessages;
+}) => {
+  return <NextIntlClientProvider locale={locale} messages={messages}>
+        <App>
+            <Navbar title="My App" />
+            {children}
+        </App>
+    </NextIntlClientProvider>
 }
