b7847d866fb6b0674a17d6ae2753ccb09d0b5247
Adeeb Shah <adeebshah4@gmail.com>
2026-05-24 00:23:15 +0530

    Feat: App store bundle name chnaged to appstore

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 import { KeyboardResize } from '@capacitor/keyboard';
 
 const config: CapacitorConfig = {
-  appId: 'com.tijarah.app',
+  appId: 'com.tijarah.appstore',
   appName: 'Tijarah',
   webDir: 'out',
   server: {
